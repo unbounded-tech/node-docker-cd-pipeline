c7f53c23b3e4a587bd7366edbf7387a044a16a3a
Patrick Scott <patrick.scott@zx-ventures.com>
2017-09-14 00:17:48 -0400

    100% test coverage

---
+++ __tests__/routes/index.js
@@ -0,0 +1,20 @@
+import indexRoute from 'routes/index'
+
+describe('routes/index', () => {
+
+  it('exports a function', () => {
+    expect(typeof indexRoute).toBe('function')
+  })
+
+  it('calls reply.send', () => {
+    const request = {}
+    const reply = {
+      send: jest.fn()
+    }
+
+    indexRoute(request, reply)
+
+    expect(reply.send).toBeCalled()
+  })
+
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import server from './lib/server'
 import onServerListen from './lib/onServerListen'
+import indexRoute from './routes/index'
+
 // Declare a route
-server.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
+server.get('/', indexRoute)
 
 // Run the server!
 server.listen(3000, onServerListen)
